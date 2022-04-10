4945f9febc1ac6c87bf90ad66f64b7a558d761f6
LAPTOP-B4KEAA9R\紫藤萝 <13650787520@139.com>
2022-04-10 12:34:04 +0800

    操作添加用户信息

--- service/api/router.js
+++ service/api/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const token = require('./token.js');
+const moment = require('moment');
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -14,8 +15,8 @@ connection.connect();
 // 账号密码登录
 router.post('/getGoodsList', (req, res) => {
     // 定义查询 sql
-    const sel_user_sql =  `select * from goods`;
-    connection.query(sel_user_sql, (err, results) => {
+    const get_goods =  `select * from goods`;
+    connection.query(get_goods, (err, results) => {
         if (err) throw err;
         if (!results) {
             res.json({
@@ -33,13 +34,22 @@ router.post('/getGoodsList', (req, res) => {
 })
 // 加入购物车
 router.post('/addShopCar', (req, res) => {
+    const userName = req.headers.username
     const params = req.body;
-    const get_goods = `select * from shopcar where goodsId = '${params.goodsId}'`;
-    connection.query(get_goods, (err, results) => {
+    const add_shop_car = `select * from shopcar where goodsId = '${params.goodsId}' and userName='${userName}'`;
+    connection.query(add_shop_car, (err, results) => {
         let sql = "";
         if(results.length == 0) {   // 购物车没有该数据，则新增，否则仅修改购买数量
-            sql =  `INSERT INTO shopcar(goodsId, goodsName, goodsPrice, goodsUnit, goodsImgUrl, orderNum) 
-            values('${params.goodsId}','${params.goodsName}',${params.goodsPrice},'${params.goodsUnit}','${params.goodsImgUrl}',${params.orderNum});`;
+            sql =  `INSERT INTO shopcar(goodsId, goodsName, goodsPrice, goodsUnit, goodsImgUrl, orderNum, userName) 
+            values(
+                '${params.goodsId}',
+                '${params.goodsName}',
+                ${params.goodsPrice},
+                '${params.goodsUnit}',
+                '${params.goodsImgUrl}',
+                ${params.orderNum},
+                '${userName}'
+            );`;
         } else {
             const stockNum = results[0].orderNum; // 库存数据 
             const newNum = stockNum * 1 + params.orderNum * 1
@@ -62,9 +72,10 @@ router.post('/addShopCar', (req, res) => {
 // 查询购物车数据
 router.post('/getShopCar', (req, res) => {
     const reqBody = req.body;
+    const userName = req.headers.username
     // 定义查询 sql
-    const sel_user_sql =  `select * from shopcar`;
-    connection.query(sel_user_sql, (err, results) => {
+    const get_shop_car =  `select * from shopcar where userName = '${userName}'`;
+    connection.query(get_shop_car, (err, results) => {
         if (err) throw err;
         if (!results) {
             res.json({status: '-1', message: '查询错误'})
@@ -76,9 +87,10 @@ router.post('/getShopCar', (req, res) => {
 // 移出购物车
 router.post('/removeShopCar', (req, res) => {
     const reqBody = req.body;
+    const userName = req.headers.username
     // 定义查询 sql
-    const sel_user_sql =  `delete from shopcar where goodsId in (${reqBody.goodsIdStr})`;
-    connection.query(sel_user_sql, (err, results) => {
+    const remove_shop_car =  `delete from shopcar where goodsId in (${reqBody.goodsIdStr}) and userName = '${userName}'`;
+    connection.query(remove_shop_car, (err, results) => {
         if (err) throw err;
         if (!results) {
             res.json({status: '-1', message: '查询错误'})
@@ -90,8 +102,11 @@ router.post('/removeShopCar', (req, res) => {
 // 生成订单
 router.post('/addOrder', (req, res) => {
     const reqBody = req.body;
-    const sel_user_sql =  `select * from shopcar where goodsId in (${reqBody.goodsIdStr})`;
-    connection.query(sel_user_sql, (err, results) => {
+    const userName = req.headers.username
+    const orderDate = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log(`orderDate='${orderDate}'`);
+    const add_order =  `select * from shopcar where goodsId in (${reqBody.goodsIdStr}) and userName = '${userName}'`;
+    connection.query(add_order, (err, results) => {
         if (err) throw err;
         if (results[0] === undefined) {
             res.json({status: '-1',message: '购物车查询不到该产品信息！'})
@@ -99,14 +114,14 @@ router.post('/addOrder', (req, res) => {
             let addValue = "";
             for(let index in results) {
                 const item = results[index];
-                let p = `(${reqBody.orderCode},'${item.goodsId}','${item.goodsName}',${item.goodsPrice},'${item.goodsUnit}','${item.goodsImgUrl}',${item.orderNum},'暂无')`
+                let p = `(${reqBody.orderCode},'${item.goodsId}','${item.goodsName}',${item.goodsPrice},'${item.goodsUnit}','${item.goodsImgUrl}',${item.orderNum},'${orderDate}','${userName}')`
                 if(index == 0) {
                     addValue = p;
                 } else {
                     addValue+=`,${p}`;
                 }
             }
-            const add_order = `INSERT INTO goodsorder(orderCode, goodsId, goodsName, goodsPrice, goodsUnit, goodsImgUrl, orderNum, orderUser) values ${addValue}`;
+            const add_order = `INSERT INTO goodsorder(orderCode, goodsId, goodsName, goodsPrice, goodsUnit, goodsImgUrl, orderNum, orderDate, userName) values ${addValue}`;
             connection.query(add_order, (err, results) => {
                 if (err) throw err;
                 if (!results) {
@@ -137,44 +152,13 @@ router.post('/loginByUserName', (req, res) => {
         } else {
             if (results[0].userName === user.userName && results[0].password === user.password) {
                 const userToken = token.createToken(user);
-                res.json({
+                const rspJson = {
                     status: '0',
                     message: '登陆成功！',
-                    token: userToken
-                })
-            } else {
-                res.json({
-                    status: '1',
-                    message: '密码错误！'
-                })
-            }
-        }
-    })
-})
-
-// 登录模块
-router.post('/login', (req, res) => {
-    const user = req.body;
-    console.log("req.body")
-    console.log(req.body);
-    // 定义查询 sql
-    const sel_email_sql =  `select * from user where email = '${user.email}'`;
-    connection.query(sel_email_sql, (err, results) => {
-        if (err) throw err;
-        // console.log(results);
-        if (results[0] === undefined) {
-            res.json({
-                status: '-1',
-                message: '邮箱填写错误，用户不存在！'
-            })
-        } else {
-            if (results[0].email === user.email && results[0].password === user.password) {
-                const userToken = token.createToken(user);
-                res.json({
-                    status: '0',
-                    message: '登陆成功！',
-                    token: userToken
-                })
+                    token: userToken,
+                    userName: user.userName
+                }
+                res.json(rspJson)
             } else {
                 res.json({
                     status: '1',
@@ -188,13 +172,10 @@ router.post('/login', (req, res) => {
 // 注册模块
 router.post('/add', (req, res) => {
     const register = req.body;
-    // console.log(register);
     // 查询 sql 防止一个邮箱重复注册
     const sel_sql = `select * from user where email = '${register.email}'`;
-
     // 添加 sql
     const add_sql = 'insert into user(email, password) values (?, ?)';
-
     connection.query(sel_sql, (err, results) => {
         if (err) throw err;
         if (results.length !== 0 && register.email === results[0].email) {
